3026acfd2aeea0f8da003d7cf72211fb1dfe905d
linux_drocher_wsl <pocofcustom@gmail.com>
2025-12-29 10:51:36 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+
+bot = telebot.TeleBot(TOKEN)
+
 @bot.message_handler(content_types=['text', 'photo', 'video', 'document'])
 def forward_to_channel(message):
-    print("Получено сообщение от:", message.chat.id, message.chat.type)
+    user_info = f"{message.from_user.first_name} {message.from_user.last_name or ''} (@{message.from_user.username or 'no_username'})"
+    print(f"Сообщение от: {user_info}, chat_id: {message.chat.id}, type: {message.chat.type}")
+
     if message.chat.type == "private":
-        bot.copy_message(
-            chat_id=CHANNEL_ID,
-            from_chat_id=message.chat.id,
-            message_id=message.message_id
-        )
+        try:
+            bot.copy_message(
+                chat_id=CHANNEL_ID,
+                from_chat_id=message.chat.id,
+                message_id=message.message_id
+            )
+            print("Сообщение успешно отправлено в канал ✅")
+        except Exception as e:
+            print("Ошибка при отправке в канал:", e)
+
+bot.infinity_polling()
